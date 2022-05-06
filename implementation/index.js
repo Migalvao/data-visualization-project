044f68e73e85edafe19d27c170c6f8db7f85ce55
Miguel Galvão <miguelepgalvao@gmail.com>
2022-05-06 04:22:12 +0100

    stations and connections rendered

--- implementation/index.js
+++ implementation/index.js
@@ -1,53 +1,31 @@
-var width = 700,
-    height = 540,
+var width = window.innerWidth - 100,
+    height = window.innerHeight - 80;
     scale = 270000,
-    latitude = 37.7750,
-    longitude = -122.4183;
+    latitude = 37.7880,
+    longitude = -122.4153;
 
-var svg = d3.select("svg")
-    .attr("width", width)
-    .attr("height", height);
+var svg;
+var stations_json = {};
 
 var projection = d3.geoAlbers()
-    .scale(scale)
+    .scale(700000)
     .rotate([-longitude, 0])
-    .center([0, latitude])
-    .parallels([24, 43]);
+    .center([0, latitude]);
+    // .parallels([24, 43]);
 
-// Create data for circles:
-var markers = [
-    { long: -122.39997, lat: 37.795001, name: "Clay at Battery" },
-    { long: -122.398436, lat: 37.79728, name: "Davis at Jackson" },
-    { long: -122.402923, lat: 37.794230999999996, name: "Commercial at Montgomery" },
-    { long: -122.40476699999999, lat: 37.795425, name: "Washington at Kearney" },
-    { long: -122.403452, lat: 37.788975, name: "Post at Kearney" },
-    { long: -122.398525, lat: 37.799953, name: "Embarcadero at Vallejo" },
-];
+// A path generator
+const path = d3.geoPath()
+.projection(projection) 
 
-// Draw map
-d3.json("SFN.geojson", function (data) {
-    svg.append("g")
-        .selectAll("path")
-        .data(data.features)
-        .enter()
-        .append("path")
-        .attr("d", d3.geoPath().projection(projection))
-        .style("fill", function () { return "#FF7870" })
-        //.on("mouseover", function (e) { d3.select(this).style("fill", "#E4EEE3") })
-        //.on("mouseout", function (e) { d3.select(this).style("fill", "#FF7870") })
-        .attr("stroke", "white")
-        .attr("stroke-width", .3)
-        .style("opacity", .3)
-});
-
-// Add circles
-svg
+const render_stations = (stations) => {
+    // Add circles
+    svg
     .selectAll("circle")
-    .data(markers)
+    .data(stations)
     .enter()
     .append("circle")
-    .attr("cx", function (d) { return projection([d.long, d.lat])[0] })
-    .attr("cy", function (d) { return projection([d.long, d.lat])[1] })
+    .attr("cx", function (d) { return projection([d.long, d.lat])[0]; })
+    .attr("cy", function (d) { return projection([d.long, d.lat])[1]; })
     .attr("r", 14)
     .style("fill", "69b3a2")
     .attr("stroke", "#69b3a2")
@@ -79,33 +57,97 @@ svg
         d3.select(this.parentNode).selectAll('#temp').remove('#temp');
         d3.select(this.parentNode).selectAll('#temp2').remove('#temp2');
     }).raise();
+}
 
-/*
-// Draw Stations
-d3.csv('station.csv')
-    .row(function (data) {
-        return {
-            name: data.name,
-            lat: parseFloat(data.lat),
-            long: parseInt(data.long),
-            origin: data.Origin,
-            city: data.city
-        };
+const render_connections = (trips, stations, trip_counts) => {
+
+    const link = []
+
+    trip_counts.forEach(function(row){
+        station_1 = stations_json[row.start_station_id];
+        station_2 = stations_json[row.end_station_id];
+
+        // console.log(row)
+        // console.log(station_1, station_2);
+        source = [+station_1.long, +station_1.lat]
+        target = [+station_2.long, +station_2.lat]
+        topush = {type: "LineString", coordinates: [source, target], size: row.count / 2500}
+
+        // console.log(topush);
+        link.push(topush)
     })
-    .get(function (data) {
-        console.log(data.city);
-        svg
-            .selectAll("stations")
-            .data(data)
-            .enter()
-            .append('circle')
-            .filter(function (d) { return d.city == "San Francisco" })
-            .attr("cx", function (d) { return projection([d.long, d.lat])[0] })
-            .attr("cy", function (d) { return projection([d.long, d.lat])[1] })
-            .attr("r", 14)
-            .style("fill", "69b3a2")
-            .attr("stroke", "#69b3a2")
-            .attr("stroke-width", 3)
-            .attr("fill-opacity", .4)
-    });
-    */
+
+
+    // Add the path
+    svg.selectAll("myPath")
+    .data(link)
+    .join("path")
+        .attr("d", function(d){ return path(d)})
+        .style("fill", "red")
+        .style("stroke", "#69b3a2")
+        .style("stroke-width", function(d){ console.log(d.size); return d.size; });
+}
+
+const render_map = (map_json) => {
+    svg.append("g")
+        .selectAll("path")
+        .data(map_json.features)
+        .enter()
+        .append("path")
+        .attr("d", d3.geoPath().projection(projection))
+        .style("fill", function () { return "#FF7870" })
+        // .on("mouseover", function (e) { d3.select(this).style("fill", "#E4EEE3") })
+        // .on("mouseout", function (e) { d3.select(this).style("fill", "#FF7870") })
+        .attr("stroke", "white")
+        .attr("stroke-width", .3)
+        .style("opacity", .3);
+}
+
+    
+const load_data = async () => {
+
+    const stations = await d3.csv("data/station.csv", function(d) { return {id: d.id, name: d.name, lat: d.lat, long: d.long}; });
+
+    const trips = await d3.csv("data/trip.csv", function(d) { return {start_date: d.start_date, end_date: d.end_date, start_station_id: d.start_station_id, end_station_id: d.end_station_id} });
+
+    const trip_counts = await d3.csv("data/trip_count.csv", function(d) { return {start_station_id: d.start_station_id, end_station_id: d.end_station_id, count: d.count} });
+
+    const station_status = await d3.csv("data/status.csv", function(d) { return { station_id: d.station_id, docks_available: d.docks_available, date_time: d.time} });
+
+    const map_json = await d3.json("SFN.geojson");
+
+    return {stations, station_status, trips, trip_counts, map_json};
+
+}
+
+const main = () => {
+
+    d3.select('body')
+        .append('svg')
+        .style('width', width)
+        .style('height', height)
+        .style('background-color', 'rgb(222, 222, 222)')
+        .style('display', 'block')
+        .style('margin', 'auto');
+
+    svg = d3.select('svg');
+
+    load_data().then(({stations, station_status, trips, trip_counts, map_json}) => {
+
+        stations.map((s) => { stations_json[s.id] = {lat: s.lat, long: s.long, name: s.name} });
+        
+        render_map(map_json);
+
+        render_connections(trip_counts, stations, trip_counts);
+        
+        render_stations(stations);
+    })
+
+}
+
+main();
+
+
+
+
+
